7f08809849397fd2f51a000f06a4c7bf9debd36c
S1m1ster <darrylrobles14@gmail.com>
2022-06-27 17:36:35 -0400

    Adding models

---
+++ ExpenseReimbursementSystem/src/main/java/com/revature/ExpenseReimbursementSystem/models/User.java
@@ -0,0 +1,86 @@
+package com.revature.ExpenseReimbursementSystem.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "username", nullable = false, length = 60)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @Column(name = "firstName", nullable = false, length = 60)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false, length = 60)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 128)
+    private String email;
+
+    @Column(name = "user_role", nullable = false)
+    private int user_role;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(int user_role) {
+        this.user_role = user_role;
+    }
+}
