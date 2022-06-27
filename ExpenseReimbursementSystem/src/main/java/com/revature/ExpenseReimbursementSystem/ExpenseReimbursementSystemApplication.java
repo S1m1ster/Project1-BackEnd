5ed9a7be77a04dcf2b88c24ef5f9658f72e77671
S1m1ster <darrylrobles14@gmail.com>
2022-06-27 13:08:10 -0400

    adding project

---
+++ ExpenseReimbursementSystem/src/main/java/com/revature/ExpenseReimbursementSystem/ExpenseReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ExpenseReimbursementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReimbursementSystemApplication.class, args);
+	}
+
+}
---
+++ ExpenseReimbursementSystem/src/test/java/com/revature/ExpenseReimbursementSystem/ExpenseReimbursementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ExpenseReimbursementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReimbursementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
